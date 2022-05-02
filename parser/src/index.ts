59bc2efd86af4e90c32c5e358c17e50d4e9afee1
Anthony Pizzurro <anthony@anthmatic.com>
2022-05-02 16:24:19 -0400

    jinja-compat is actually not needed in parsing

--- parser/src/index.ts
+++ parser/src/index.ts
@@ -6,20 +6,15 @@
  * we install full Jinja compatibility using the `nunjucks.installJinjaCompat()` function.
  * For testing, we use the `testInput` function.  For actual release, we export the `parse` function.
  */
-import nunjucks from "./parser";
+import parser from "./parser";
 import { Parser } from "./types/parser";
 import Tags from "./Tags";
-import installCompat from "./hubl-compat.js";
 
 type AST = any; //TODO Type me
 
 const parse = (text: string, parsers: object, options: object): AST => {
-  // Nunjucks exposes nunjucks.installJinjaCompat(), but this is a modified version with HubL support
   //@ts-ignore
-  installCompat(nunjucks);
-  // Nunjucks' type doesn't expose the parser, so we need to do some casting
-  //@ts-ignore
-  const { parse } = nunjucks.parser as Parser;
+  const { parse } = parser.parser as Parser;
   // We call into nunjucks' default parser, but we extend it by passing in our custom tags
   return parse(text, [new Tags()], { trimBlocks: false, lstripBlocks: false });
 };
